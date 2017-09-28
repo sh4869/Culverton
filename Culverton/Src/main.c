3ef2694267a7df994f2b151237cb0c55dc6f4cd6
sh4869 <nobuk4869@gmail.com>
2017-09-28 17:07:22 +0900

    Fix label name

--- Culverton/Src/main.c
+++ Culverton/Src/main.c
@@ -470,11 +470,11 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|MOTOR_STBY_Pin|MOTOR_RIGHT_IN2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, IFLED1_Pin|IFLED3_Pin|MOTOR_LEFT_IN1_Pin|MOTOR_LEFT_IN2_Pin 
+  HAL_GPIO_WritePin(GPIOC, IFLED4_Pin|IFLED3_Pin|MOTOR_LEFT_IN1_Pin|MOTOR_LEFT_IN2_Pin 
                           |MOTOR_RIGHT_IN1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, IFLED2_Pin|IFLED1B10_Pin|SENSORLED1_Pin|SENSORLED2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, IFLED2_Pin|IFLED1_Pin|SENSORLED1_Pin|SENSORLED2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PA2 MOTOR_STBY_Pin MOTOR_RIGHT_IN2_Pin */
   GPIO_InitStruct.Pin = GPIO_PIN_2|MOTOR_STBY_Pin|MOTOR_RIGHT_IN2_Pin;
@@ -482,16 +482,16 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : IFLED1_Pin IFLED3_Pin MOTOR_LEFT_IN1_Pin MOTOR_LEFT_IN2_Pin 
+  /*Configure GPIO pins : IFLED4_Pin IFLED3_Pin MOTOR_LEFT_IN1_Pin MOTOR_LEFT_IN2_Pin 
                            MOTOR_RIGHT_IN1_Pin */
-  GPIO_InitStruct.Pin = IFLED1_Pin|IFLED3_Pin|MOTOR_LEFT_IN1_Pin|MOTOR_LEFT_IN2_Pin 
+  GPIO_InitStruct.Pin = IFLED4_Pin|IFLED3_Pin|MOTOR_LEFT_IN1_Pin|MOTOR_LEFT_IN2_Pin 
                           |MOTOR_RIGHT_IN1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : IFLED2_Pin IFLED1B10_Pin SENSORLED1_Pin SENSORLED2_Pin */
-  GPIO_InitStruct.Pin = IFLED2_Pin|IFLED1B10_Pin|SENSORLED1_Pin|SENSORLED2_Pin;
+  /*Configure GPIO pins : IFLED2_Pin IFLED1_Pin SENSORLED1_Pin SENSORLED2_Pin */
+  GPIO_InitStruct.Pin = IFLED2_Pin|IFLED1_Pin|SENSORLED1_Pin|SENSORLED2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
