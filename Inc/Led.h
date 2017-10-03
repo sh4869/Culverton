f90476387ca0bb14e2b6be24e5f550aa2277332b
sh4869 <nobuk4869@gmail.com>
2017-10-03 11:17:15 +0900

    Fix LED Class

--- Inc/Led.h
+++ Inc/Led.h
@@ -7,16 +7,6 @@
 #ifndef LED_H_
 #define LED_H_
 
-// Defines
-#define IFLED1_Pin GPIO_PIN_10
-#define IFLED1_GPIO_Port GPIOB
-#define IFLED2_Pin GPIO_PIN_0
-#define IFLED2_GPIO_Port GPIOB
-#define IFLED3_Pin GPIO_PIN_5
-#define IFLED3_GPIO_Port GPIOC
-#define IFLED4_Pin GPIO_PIN_4
-#define IFLED4_GPIO_Port GPIOC
-
 #include <array>
 #include <utility>
 #include "stm32f1xx_hal.h"
@@ -24,19 +14,20 @@
 enum class LedNumber : int { ONE, TWO, THREE, FOUR };
 
 class Led {
- private:
-  std::array<std::pair<GPIO_TypeDef *, uint16_t>,4> gpio_pins;
-  static Led* instance;
-  Led();
-  void init();
+   private:
+    std::array<std::pair<GPIO_TypeDef*, uint16_t>, 4> gpio_pins;
+    static Led* instance;
+    Led();
+    void init();
 
- public:
-  ~Led();
-  static Led* getInstance();
-  void AllOn();
-  void AllOff();
-  void On(LedNumber num);
-  void Off(LedNumber num);
+   public:
+    ~Led();
+    static Led* getInstance();
+    void AllOn();
+    void AllOff();
+    void On(LedNumber num);
+    void Off(LedNumber num);
+    void OnOnly(LedNumber num);
 };
 
 #endif
--- Src/Led.cpp
+++ Src/Led.cpp
@@ -1,17 +1,26 @@
 #include "Led.h"
 #include <map>
+#include "stm32f1xx_hal.h"
+
+// Defines
+#define IFLED1_Pin GPIO_PIN_10
+#define IFLED1_GPIO_Port GPIOB
+#define IFLED2_Pin GPIO_PIN_0
+#define IFLED2_GPIO_Port GPIOB
+#define IFLED3_Pin GPIO_PIN_5
+#define IFLED3_GPIO_Port GPIOC
+#define IFLED4_Pin GPIO_PIN_4
+#define IFLED4_GPIO_Port GPIOC
 
 Led* Led::instance = nullptr;
 
-std::array<std::pair<GPIO_TypeDef*, uint16_t>, 4> pin_map = {
-    std::make_pair(IFLED1_GPIO_Port, IFLED1_Pin), std::make_pair(IFLED2_GPIO_Port, IFLED2_Pin),
-    std::make_pair(IFLED3_GPIO_Port, IFLED3_Pin), std::make_pair(IFLED4_GPIO_Port, IFLED4_Pin),
-};
-
 Led::Led() {}
 
-void Led::init(){
-    gpio_pins = pin_map;
+void Led::init() {
+    gpio_pins = {
+        std::make_pair(IFLED1_GPIO_Port, IFLED1_Pin), std::make_pair(IFLED2_GPIO_Port, IFLED2_Pin),
+        std::make_pair(IFLED3_GPIO_Port, IFLED3_Pin), std::make_pair(IFLED4_GPIO_Port, IFLED4_Pin),
+    };
     GPIO_InitTypeDef GPIO_InitStruct;
 
     __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -27,12 +36,10 @@ void Led::init(){
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
-Led::~Led() {
-    delete instance;
-}
+Led::~Led() { delete instance; }
 
-Led* Led::getInstance(){
-    if(instance == nullptr){
+Led* Led::getInstance() {
+    if (instance == nullptr) {
         instance = new Led();
         instance->init();
     }
@@ -40,23 +47,28 @@ Led* Led::getInstance(){
 }
 
 void Led::AllOn() {
-    for (uint i = 0; i < pin_map.size(); i++) {
+    for (uint i = 0; i < gpio_pins.size(); i++) {
         HAL_GPIO_WritePin(gpio_pins[i].first, gpio_pins[i].second, GPIO_PIN_SET);
     }
 }
 
 void Led::AllOff() {
-    for (uint i = 0; i < pin_map.size(); i++) {
+    for (uint i = 0; i < gpio_pins.size(); i++) {
         HAL_GPIO_WritePin(gpio_pins[i].first, gpio_pins[i].second, GPIO_PIN_SET);
     }
 }
 
 void Led::On(LedNumber num) {
     int index = static_cast<int>(num);
-    HAL_GPIO_WritePin(gpio_pins[index].first,gpio_pins[index].second,GPIO_PIN_SET);
+    HAL_GPIO_WritePin(gpio_pins[index].first, gpio_pins[index].second, GPIO_PIN_SET);
 }
 
-void Led::Off(LedNumber num){
+void Led::Off(LedNumber num) {
     int index = static_cast<int>(num);
-    HAL_GPIO_WritePin(gpio_pins[index].first,gpio_pins[index].second,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(gpio_pins[index].first, gpio_pins[index].second, GPIO_PIN_RESET);
+}
+
+void Led::OnOnly(LedNumber num) {
+    AllOff();
+    Led::On(num);
 }
