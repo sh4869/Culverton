d0d169d01b6b90ac2ae94aceb835147826267c92
sh4869 <nobuk4869@gmail.com>
2017-10-09 01:40:54 +0900

    Update Util.h
    * Add namespace GPIo

--- Inc/util.h
+++ Inc/util.h
@@ -1,11 +1,18 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
+#include "Types.h"
+#include "stm32f1xx_hal.h"
+#include <utility>
+
 namespace Util {
-void Delay(__IO uint32_t count) {
-    for (; count != 0; count--)
-        ;
+    void Delay(__IO uint32_t count);
 }
+// GPIO.cを移動させられたらそこに移動する
+
+namespace GPIO {
+    void On(GPIOPin pin);
+    void Off(GPIOPin pin);
 }
 
 #endif
