6b4bb48def8ec41ae014903080b0d6f623ebdddc
sh4869 <nobuk4869@gmail.com>
2017-10-14 17:38:24 +0900

    Add Variables.h

---
+++ Inc/Variables.h
@@ -0,0 +1,14 @@
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+
+// 物理定数系
+static constexpr float GearRatio = 8.0F / 42.0F;
+static constexpr float WheelRadius = 27.0F / 2.0F;
+static constexpr float EncoderPulse = 1024.0F;
+static constexpr float PI = 3.14159265258979F;
+
+// コース系
+static constexpr float WallWidth = 180.0F; // mm
+static constexpr float PoleWidth = 12.0F; // mm
+#endif
