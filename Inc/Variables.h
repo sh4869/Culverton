eb5edcbb99bd4cb3fec19c0a244245d7342ff416
sh4869 <nobuk4869@gmail.com>
2017-11-05 17:32:48 +0900

    Add Comment and remove PI(Predefined)

--- Inc/Variables.h
+++ Inc/Variables.h
@@ -1,12 +1,19 @@
 #ifndef VARIABLES_H_
 #define VARIABLES_H_
 
-
 // 物理定数系
+/**
+ * ギア比
+ */ 
 static constexpr float GearRatio = 8.0F / 42.0F;
+/**
+ * タイヤ半径
+ */ 
 static constexpr float WheelRadius = 27.0F / 2.0F;
+/**
+ * エンコーダの1回転あたりのパルス数
+ */
 static constexpr float EncoderPulse = 1024.0F;
-static constexpr float PI = 3.14159265258979F;
 
 // コース系
 static constexpr float WallWidth = 180.0F; // mm
