0e5240a323675c735645fe1926beaf5cb755f32b
sh4869 <nobuk4869@gmail.com>
2017-11-16 15:48:03 +0900

    Update MouseSystem::startMouse

--- Src/Common/MouseSystem.cpp
+++ Src/Common/MouseSystem.cpp
@@ -13,32 +13,24 @@ void MouseSystem::init() {
     MX_ADC2_Init();
     MX_ADC3_Init();
     MX_TIM8_Init();
-    MX_TIM5_Init();
-    initPeripheral();
 }
 
-void MouseSystem::initPeripheral() {
-    led = Led::GetInstance();
-    sensor = Sensor::GetInstance();
-    buzzer = Buzzer::GetInstance();
-    bm = BatteryMonitor::GetInstance();
-    uart = Uart::GetInstance();
-}
+void MouseSystem::initPeripheral() {}
 
 // パフォーマンス的な
 void MouseSystem::StartMouse() {
+    static Led *led = Led::GetInstance();
+    static Buzzer *buzzer = Buzzer::GetInstance();
     // Start Perfomnace
-    buzzer->On(1000);
+    buzzer->On(2500);
     led->AllOn();
     HAL_Delay(500);
     buzzer->Off();
     led->AllOff();
     HAL_Delay(100);
-    for (int j = 0; j < 2; j++) {
-        for (int i = 0; i < 4; i++) {
-            led->OnOnly(static_cast<LedNumber>(i));
-            HAL_Delay(200);
-        }
+    for (int i = 0; i < 4; i++) {
+        led->OnOnly(static_cast<LedNumber>(i));
+        HAL_Delay(200);
     }
     led->AllOn();
     HAL_Delay(1000);
@@ -47,6 +39,7 @@ void MouseSystem::StartMouse() {
 }
 
 void MouseSystem::BatteryCheck() {
+    static BatteryMonitor *bm = BatteryMonitor::GetInstance();
     if ((bm->GetValue() * (8.0F / 5.0F)) < 6.0F) {
         char str[1000];
         while ((bm->GetValue() * (8.0F / 5.0F)) < 6.0F) {
