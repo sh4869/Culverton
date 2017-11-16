e09379148161056e8229338c04bb0ccfdf51152e
sh4869 <nobuk4869@gmail.com>
2017-11-17 03:40:19 +0900

    Add Constrcutor for Target

--- Inc/Common/Target.h
+++ Inc/Common/Target.h
@@ -1,14 +1,18 @@
 #ifndef TARGET_H_
 #define TARGET_H_
 
-#include "Velocity.h"
 #include "Position.h"
+#include "Velocity.h"
 
 struct Target {
     enum class TargetVariety { STRAIGHT, PIVOTTURN, STOP };
     TargetVariety variety;
     Velocity ve;
     Position pos;
+    Target(TargetVariety _variety = TargetVariety::STOP,
+           Velocity _v = { 0, 0 },
+           Position _pos = { 0, 0 })
+        : variety(_variety), ve(_v), pos(_pos) {}
 };
 
 #endif
